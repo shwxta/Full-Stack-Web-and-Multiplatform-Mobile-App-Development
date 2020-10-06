76d19d0465489b80a857d522976599370e4198d5
shwxta <shwxta@gmail.com>
2020-10-06 20:49:07 +0530

    single page applications part 1

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -14,6 +14,7 @@ import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
@@ -42,7 +43,7 @@ import { ContactComponent } from './contact/contact.component';
     MatCardModule,
     MatButtonModule,
   ],
-  providers: [DishService],
+  providers: [DishService, PromotionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

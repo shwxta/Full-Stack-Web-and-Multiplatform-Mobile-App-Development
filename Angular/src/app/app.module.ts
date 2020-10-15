1f1aa77d801daf7101d97543455d657e0ec2497e
shwxta <shwxta@gmail.com>
2020-10-15 12:55:01 +0530

    template forms part 1

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -23,6 +23,10 @@ import { ContactComponent } from './contact/contact.component';
 import { LeaderService } from './services/leader.service';
 import { LoginComponent } from './login/login.component';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms'; 
 
 @NgModule({
   declarations: [
@@ -47,6 +51,10 @@ import { MatDialogModule } from '@angular/material/dialog';
     MatCardModule,
     MatButtonModule,
     MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule,
+    FormsModule,
   ],
   providers: [DishService, PromotionService, LeaderService],
   entryComponents: [
